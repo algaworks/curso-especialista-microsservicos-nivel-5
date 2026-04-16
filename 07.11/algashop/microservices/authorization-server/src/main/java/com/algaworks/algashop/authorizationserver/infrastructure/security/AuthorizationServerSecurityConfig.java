08833aef9ed0fe9e8596ad421078e0aae220cc91
alexaugustobr <alexaugustoti@gmail.com>
2026-04-16 10:25:31 -0300

    modulo 7 e 8

---
+++ 07.11/algashop/microservices/authorization-server/src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/AuthorizationServerSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.algaworks.algashop.authorizationserver.infrastructure.security;
+
+import com.algaworks.algashop.authorizationserver.infrastructure.security.oidc.OidcUserInfoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.oidc.web.authentication.OidcLogoutAuthenticationSuccessHandler;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthorizationServerSecurityConfig {
+
+	private final OidcUserInfoMapper oidcUserInfoMapper;
+	private final OidcLogoutAuthenticationSuccessHandler oidcLogoutAuthenticationSuccessHandler;
+
+	@Bean
+	@Order(1)
+	public SecurityFilterChain authorizationServerFilterChain(HttpSecurity http) {
+		var authorizationServer = new OAuth2AuthorizationServerConfigurer();
+
+		http.securityMatcher(authorizationServer.getEndpointsMatcher())
+				.with(authorizationServer, configurer ->
+						configurer.oidc(oidc -> oidc
+								.logoutEndpoint(logout ->
+										logout.logoutResponseHandler(oidcLogoutAuthenticationSuccessHandler))
+								.userInfoEndpoint(
+									userInfo -> userInfo.userInfoMapper(oidcUserInfoMapper))))
+				.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+				.exceptionHandling(
+						exceptions -> exceptions.defaultAuthenticationEntryPointFor(
+								new LoginUrlAuthenticationEntryPoint("/login"),
+								new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+						)
+				);
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+				.formLogin(Customizer.withDefaults());
+		return http.build();
+	}
+
+}
