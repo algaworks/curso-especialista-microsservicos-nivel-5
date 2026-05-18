98df19326f1e8fa9f8d8ddc0e3b8dc306fec515b
alexaugustobr <alexaugustoti@gmail.com>
2026-05-18 11:59:11 -0300

    novas aulas n5

---
+++ 12.03-testes-para-garantir-regras-de-seguranca/algashop/microservices/authorization-server/src/main/java/com/algaworks/algashop/authorizationserver/application/user/management/AuthUserManagementApplicationService.java
@@ -0,0 +1,81 @@
+package com.algaworks.algashop.authorizationserver.application.user.management;
+
+import com.algaworks.algashop.authorizationserver.application.security.SecurityChecks;
+import com.algaworks.algashop.authorizationserver.application.user.query.AuthUserNotFoundException;
+import com.algaworks.algashop.authorizationserver.application.user.query.AuthUserOutput;
+import com.algaworks.algashop.authorizationserver.domain.model.user.AuthUser;
+import com.algaworks.algashop.authorizationserver.domain.model.user.AuthUserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthUserManagementApplicationService {
+
+	private final AuthUserRepository authUserRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final SecurityChecks securityCheck;
+
+	public AuthUserOutput create(AuthUserInput input) {
+		if (!securityCheck.canRegisterUserOfType(input.getType())) {
+			throw new AccessDeniedException("Cannot register user of type " + input.getType());
+		}
+
+		if (authUserRepository.existsByEmail(input.getEmail())) {
+			throw new AuthUserEmailAlreadyInUseException(input.getEmail());
+		}
+
+		String tempPassword = RandomStringUtils.secure().nextAlphanumeric(12);
+
+		System.out.println(tempPassword); //todo send via email
+
+		String passwordHash = passwordEncoder.encode(tempPassword);
+
+		AuthUser user = AuthUser.brandNew(
+				input.getEmail(),
+				input.getName(),
+				input.getType(),
+				passwordHash
+		);
+
+		return AuthUserOutput.from(authUserRepository.save(user));
+	}
+
+	public AuthUserOutput update(UUID userId, AuthUserUpdateInput input) {
+		AuthUser user = authUserRepository.findById(userId)
+				.orElseThrow(() -> new AuthUserNotFoundException(userId));
+
+		verifyCanEditUser(user, input);
+
+		user.setName(input.getName());
+		user.setType(input.getType());
+		user.setEnabled(input.isEnabled());
+
+		return AuthUserOutput.from(authUserRepository.save(user));
+	}
+
+	private void verifyCanEditUser(AuthUser authUser, AuthUserUpdateInput input) {
+		if (!securityCheck.canEditUser(authUser.getType(), authUser.getId())) {
+			throw new AccessDeniedException("Cannot edit user of type " + authUser.getType());
+		}
+
+		if (!securityCheck.canChangeUserType(authUser.getType(), input.getType())) {
+			throw new AccessDeniedException("Cannot change user type to " + input.getType());
+		}
+	}
+
+	public void delete(UUID userId) {
+		AuthUser user = authUserRepository.findById(userId)
+				.orElseThrow(() -> new AuthUserNotFoundException(userId));
+		user.anonymize();
+		authUserRepository.save(user);
+	}
+
+}
---
+++ 13.05-funcionalidade-esqueci-minha-senha/algashop/microservices/ordering/src/test/java/com/algaworks/algashop/ordering/utils/MockJwtFactory.java
@@ -0,0 +1,83 @@
+package com.algaworks.algashop.ordering.utils;
+
+import org.mockito.Mockito;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MockJwtFactory {
+
+    public static final String DEFAULT_ISSUER_URI = "http://algashop-authorization-server:8081";
+
+    public static final String[] DEFAULT_SCOPES = new String[] {
+            "orders:read",
+            "orders:write",
+            "shopping-carts:read",
+            "shopping-carts:write",
+            "customers:read",
+            "customers:write"
+    };
+
+    public static final String DEFAULT_SUBJECT = "6e148bd5-47f6-4022-b9da-07cfaa294f7a";
+
+    public static final String DEFAULT_TOKEN_VALUE = "fake.jwt.token";
+
+    public static final String NO_SCOPE_TOKEN_VALUE = "fake.jwt.no-scope";
+
+    public static final String EXPIRED_TOKEN_VALUE = "fake.jwt.expired";
+
+    public static final String DEFAULT_ROLE = "CUSTOMER";
+
+    public static final String[] DEFAULT_AUDIENCES = {"ecommerce-web-app"};
+
+    public static JwtDecoder createMockJwtDecoder() {
+        JwtDecoder jwtDecoder = Mockito.mock(JwtDecoder.class);
+
+        Mockito.when(jwtDecoder.decode(DEFAULT_TOKEN_VALUE))
+                .thenReturn(buildDefaultJwt());
+
+        Mockito.when(jwtDecoder.decode(NO_SCOPE_TOKEN_VALUE))
+                .thenReturn(buildNoScopeJwt());
+
+        Mockito.when(jwtDecoder.decode(EXPIRED_TOKEN_VALUE))
+                .thenThrow(new JwtException("Token is expired"));
+
+        return jwtDecoder;
+    }
+
+    public static Jwt buildJwt(String tokenValue, String subject,
+                               String issuer, String[] scopes, String role, String[] audiences) {
+        Instant now = Instant.now();
+        Instant expiresAt = now.plusSeconds(600);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", subject);
+        claims.put("iss", issuer);
+        claims.put("role", role);
+        claims.put("aud", List.of(audiences));
+        claims.put("scope", List.of(scopes));
+
+        return Jwt.withTokenValue(tokenValue)
+                .issuedAt(now)
+                .expiresAt(expiresAt)
+                .issuer(issuer)
+                .subject(subject)
+                .claims(c -> c.putAll(claims))
+                .headers(h -> h.put("alg", "none"))
+                .build();
+    }
+
+    public static Jwt buildDefaultJwt() {
+        return buildJwt(DEFAULT_TOKEN_VALUE, DEFAULT_SUBJECT, DEFAULT_ISSUER_URI, DEFAULT_SCOPES, DEFAULT_ROLE, DEFAULT_AUDIENCES);
+    }
+
+    public static Jwt buildNoScopeJwt() {
+        return buildJwt(NO_SCOPE_TOKEN_VALUE, DEFAULT_SUBJECT, DEFAULT_ISSUER_URI, new String[]{}, DEFAULT_ROLE, DEFAULT_AUDIENCES);
+    }
+
+}
